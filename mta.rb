5dd2aecd4d66b6e982148c53ed47a5ca3113ada3
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-03-31 09:36:44 -0400

    takes line and station inputs and tells stops to transfer, and then stops to destination

--- mta.rb
+++ mta.rb
@@ -1,15 +1,31 @@
-
+	
 n = ["timessquare", "34th","28th","23rd","unionsquare","8th"]
+l = ["8th", "6th", "unionsquare","3rd","1st"]
+six = ["grandcentral","33rd","28th","23rd","unionsquare","astorplace"]
+
+subway = {:n=>n, :l=> l, :six=>six}
+
 
 puts "Where would you like to go?"
 destination = gets.strip.chomp.downcase.delete(" ")
 
+puts "What line is that on?"
+endline = gets.strip.chomp.downcase.delete(" ")
+
+
+
+
 puts "What station will you be departing from?"
 location = gets.strip.chomp.downcase.delete(" ")
 
-# puts n.index(destination)
-# puts location
-# puts destination
 
-puts "You have #{(n.index(destination) - n.index(location)).abs} stops to go"
+puts "What line is that on?"
+startline = gets.strip.chomp.downcase.delete(" ")
+startline.to_sym
 
+if startline == endline
+	puts (startline.index(destination)-startline.index(location)).abs
+else
+	puts "You have #{(subway[startline.to_sym].index(location) - subway[startline.to_sym].index("unionsquare")).abs} stops until transfer"
+	puts "After, you have #{(subway[endline.to_sym].index(destination) - subway[endline.to_sym].index("unionsquare")).abs} until your destination"	
+end    
